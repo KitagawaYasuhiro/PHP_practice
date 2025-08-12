e65bb5e796eadc3eb8c2076f433efceb1b98f3ac
yasuhiro kitagawa <yassun0827@gmail.com>
2025-08-12 21:54:59 +0900

    section6-1まで完了

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,9 +1,40 @@
 <?php
 // Q1 tic-tac問題
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 4 === 0 && $i % 5 === 0) {
+        echo "tic-tac\n";
+    } elseif ($i % 4 === 0) {
+        echo "tic\n";
+    } elseif ($i % 5 === 0) {
+        echo "tac\n";
+    } else {
+        echo $i . "\n";
+    }
+}
 
 
 // Q2 多次元連想配列
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
 
+var_dump($personalInfos);
+
+                                                                                                                                                                                                                                                                                
 
 // Q3 オブジェクト-1
 
