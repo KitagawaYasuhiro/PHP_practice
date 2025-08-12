d7201a80b8395a35fb98bec7c49fd161de7f3ad3
yasuhiro kitagawa <yassun0827@gmail.com>
2025-08-13 01:25:44 +0900

    section6-4まで完了

--- php-practice_2.php
+++ php-practice_2.php
@@ -34,13 +34,71 @@ $personalInfos = [
 
 var_dump($personalInfos);
 
-                                                                                                                                                                                                                                                                                
+// 問題1
+echo $personalInfos[1]['name'] . "の電話番号は" . $personalInfos[1]['tel'] . "です。\n";
+
+// 問題2
+foreach ($personalInfos as $index => $info) {
+    $number = $index + 1;
+    echo "{$number}番目の{$info['name']}さんのメールアドレスは{$info['mail']}で、電話番号は{$info['tel']}です。\n";
+}
+
+// 問題3
+$ageList = [25, 30, 18];
+
+foreach ($ageList as $index => $age) {
+    $personalInfos[$index]['age'] = $age;
+}
+
+var_dump($personalInfos);
 
 // Q3 オブジェクト-1
+class Student
+{
+    public $studentId;
+    public $studentName;
 
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend()
+    {
+        echo '授業に出席しました。';
+    }
+}
+
+$yamada = new Student(120, '山田');
+echo "学籍番号{$yamada->studentId}番の生徒は{$yamada->studentName}です。\n";
 
 // Q4 オブジェクト-2
+class Student
+{
+    public $studentId;
+    public $studentName;
 
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend($class)
+    {
+        echo "{$this->studentName}は{$class}の授業に出席しました。\n";
+    }
+}
+
+$yamada = new Student(120, '山田');
+$yamada->attend('PHP');
 
 // Q5 定義済みクラス
+// 問題1
+
+
+// 問題2
+
+
 ?>
